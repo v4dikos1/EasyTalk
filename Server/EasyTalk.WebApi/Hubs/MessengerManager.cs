5fd7d4c75f17c1495f26b5761c05377d9a729362
v4dikos1 <v4dikos@yandex.ru>
2023-03-14 11:37:55 +0700

    Добавлен хаб для отправки сообщений в реальном времени

---
+++ Server/EasyTalk.WebApi/Hubs/MessengerManager.cs
@@ -0,0 +1,60 @@
+﻿namespace EasyTalk.WebApi.Hubs
+{
+    public class MessengerManager
+    {
+        public List<ChatUser> Users { get; } = new();
+
+        public void ConnectUser(Guid userId, string connectionId)
+        {
+            var userAlreadyExists = GetConnectedUserByUsername(userId);
+            if (userAlreadyExists != null)
+            {
+                userAlreadyExists.AddConnection(connectionId);
+                return;
+            }
+
+            var user = new ChatUser(userId);
+            user.AddConnection(connectionId);
+            Users.Add(user);
+        }
+
+        public bool DisconnectUser(string connectionId)
+        {
+            var userExists = GetConnectedUserById(connectionId);
+            if (userExists == null)
+            {
+                return false;
+            }
+
+            if (!userExists.Connections.Any())
+            {
+                return false;
+            }
+
+            var connectionExists = userExists.Connections.Select(c => c.ConnectionId).First().Equals(connectionId);
+            if (!connectionExists)
+            {
+                return false;
+            }
+
+            if (userExists.Connections.Count() == 1)
+            {
+                Users.Remove(userExists);
+                return true;
+            }
+
+            userExists.RemoveConnection(connectionId);
+            return false;
+        }
+
+        private ChatUser? GetConnectedUserById(string connectionId)
+        {
+            return Users.FirstOrDefault(u => u.Connections.Any(c => c.ConnectionId.Equals(connectionId)));
+        }
+
+        private ChatUser? GetConnectedUserByUsername(Guid userId)
+        {
+            return Users.FirstOrDefault(u => u.Id.Equals(userId));
+        }
+    }
+}
